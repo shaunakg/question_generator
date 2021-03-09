c65d246b81f71ddcf70492c0de968dbe7368c9ef
Shaunak G <hello@srg.codes>
2021-03-09 12:22:23 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from setuptools import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(install_requires=required)
+setup(name="questiongenerator", install_requires=required)
